19f29773cad154a523eced540267b9af6072c185
niels hove <vognstang@gmail.com>
2019-01-14 11:26:54 +0100

    Initial Commit

---
+++ textapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  textapp
+//
+//  Created by niels hove on 1/14/19.
+//  Copyright © 2019 niels hove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
